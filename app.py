645da9fa4a82d52425e9e52dfeb49da676192ccd
divyanshi555 <purohitdivyanshi25@gmail.com>
2025-09-30 00:16:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,4 +133,6 @@ def upload_file():
 # Run Flask app
 # ---------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port
+    app.run(host="0.0.0.0", port=port)
+
